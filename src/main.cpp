d421e82b9ce62aafb4d9267cedbb7c881b5f572f
Maks Makuta <makuta.maks.mmc@gmail.com>
2025-03-23 09:52:49 +0200

    [0.1.2] black window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
 
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Overlay Example", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
