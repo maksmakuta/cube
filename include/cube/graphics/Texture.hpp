7b8bf98b4eb559c93514ac2dd583861e82d916ee
Maks Makuta <makuta.maks.mmc@gmail.com>
2025-04-23 12:57:53 +0300

    [0.2.1] Texture class implementation

--- include/cube/graphics/Texture.hpp
+++ include/cube/graphics/Texture.hpp
@@ -1,12 +1,62 @@
 #ifndef TEXTURE_HPP
 #define TEXTURE_HPP
 
+#include <string>
+#include <glm/glm.hpp>
+
 namespace cube {
 
-    class Texture {
-
+    enum class TextureWrap {
+        Repeat,
+        RepeatMirror,
+        ClampEdge,
+        ClampBorder
     };
 
+    enum class TextureFilter {
+        Linear,
+        Nearest
+    };
+
+    enum class TextureMinFilter {
+        LinearLinear,
+        LinearNearest,
+        NearestLinear,
+        NearestNearest
+    };
+
+    enum class TextureFormat {
+        RGBA,
+        RGB,
+        Mono
+    };
+
+    class Texture {
+    public:
+        Texture() = default;
+
+        explicit Texture(const std::string& path);
+        ~Texture();
+
+        void alloc(int w, int h, TextureFormat format,const void* buffer = nullptr);
+        void subdata(int x, int y,int w, int h, TextureFormat format, const void* buffer) const;
+        void setWrap(TextureWrap st) const;
+        void setWrap(TextureWrap s,TextureWrap t) const;
+        void setBorder(const glm::vec4& color) const;
+        void setMinFilter(TextureFilter) const;
+        void setMinFilter(TextureMinFilter) const;
+        void setMagFilter(TextureFilter) const;
+        void genMipmaps() const;
+
+        void bind(u_int8_t slot = 0) const;
+
+        [[nodiscard]] glm::uint getHandle() const;
+        [[nodiscard]] glm::uvec2 getSize() const;
+
+    private:
+        glm::uvec2 m_size{};
+        glm::uint m_handle{0};
+    };
 }
 
 #endif //TEXTURE_HPP
