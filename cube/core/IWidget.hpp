f596366b6c27d896875216d11942f500f5abff4c
Maks Makuta <makuta.maks.mmc@gmail.com>
2025-10-10 22:14:37 +0300

    [0.4.3] updated interfaces for layout and widget

--- cube/core/IWidget.hpp
+++ cube/core/IWidget.hpp
@@ -2,6 +2,8 @@
 #define CUBE_IWIDGET_HPP
 
 #include "graphics/RendererUI.hpp"
+#include "ui/LayoutParams.hpp"
+#include "ui/Rect.hpp"
 #include "window/Event.hpp"
 
 namespace cube {
@@ -21,37 +23,17 @@ namespace cube {
         virtual void onDraw(RendererUI&) = 0;
         virtual void onEvent(const Event&) = 0;
 
-        void setPos(const glm::vec2& p) {
-            m_pos = p;
-        }
-
-        void setSize(const glm::vec2& s) {
-            m_size = s;
-        }
-
-        [[nodiscard]] glm::vec2 getPos() const {
-            return m_pos;
-        }
-
-        [[nodiscard]] glm::vec2 getSize() const {
-            return m_size;
-        }
-
-        void setVisible(const bool v) { m_visible = v; }
-        void setEnabled(const bool e) { m_enabled = e; }
-
-        [[nodiscard]] bool isVisible() const { return m_visible; }
-        [[nodiscard]] bool isEnabled() const { return m_enabled; }
-
+        [[nodiscard]] WidgetPtr getParent() const { return m_parent; }
+        [[nodiscard]] const LayoutParams& getLayoutParams() const { return m_params; }
+        [[nodiscard]] const Rect& getRect() const { return m_rect; }
 
         void setParent(const WidgetPtr& p) { m_parent = p; }
-        [[nodiscard]] WidgetPtr getParent() const { return m_parent; }
+        void setLayoutParams(const LayoutParams& p) { m_params = p; }
+        void setRect(const Rect& r) { m_rect = r; }
 
     protected:
-        glm::vec2 m_pos{};
-        glm::vec2 m_size{};
-        bool m_visible = true;
-        bool m_enabled = true;
+        Rect m_rect;
+        LayoutParams m_params{};
         WidgetPtr m_parent = nullptr;
     };
 
