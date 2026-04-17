c4c1ac9923127c71409ae3e229164fed385c62cf
Maks Makuta <maksmakuta@proton.me>
2026-04-17 13:53:32 +0300

    [0.0.2] window and architecture | import glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,41 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+#include "glad/glad.h"
+
 int main() {
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        return -1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_Window* window = SDL_CreateWindow("VoxelCore-23", 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    const SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(SDL_GL_GetProcAddress))) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) running = false;
+        }
+
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DestroyContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
