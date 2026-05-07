02b31a6bef29b52ad1b98d995ebffb0b9c616f7d
Maks Makuta <maksmakuta@proton.me>
2026-05-07 12:17:27 +0300

    [0.0.10] define chunk status enum to make sure current state of chunk

---
+++ include/cube/core/ChunkStatus.hpp
@@ -0,0 +1,20 @@
+#ifndef CUBE_CHUNKSTATUS_HPP
+#define CUBE_CHUNKSTATUS_HPP
+
+namespace cube {
+
+    enum class ChunkStatus {
+        Empty,          // new chunk, no data
+        QueuedGen,      // data is generated soon
+        GenerateData,   // generating
+        Generated,      // data is present
+        QueuedMesh,     // data is meshed soon
+        Mesh,           // meshing
+        Meshed,         // mesh is done
+        Active,         // ready to render
+        Unload          // need to be removed
+    };
+
+}
+
+#endif //CUBE_CHUNKSTATUS_HPP
