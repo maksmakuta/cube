70bd1a5c9003cf88d8ce27da50a544b2e5f82ed5
Maks Makuta <maksmakuta@proton.me>
2026-05-07 11:36:29 +0300

    [0.0.6] define Block types

---
+++ include/cube/core/Block.hpp
@@ -0,0 +1,20 @@
+#ifndef CUBE_BLOCK_HPP
+#define CUBE_BLOCK_HPP
+
+#include <cstdint>
+
+namespace cube {
+
+    enum class Block : uint8_t {
+        Air = 0,
+
+        Grass,
+        Dirt,
+        Stone,
+
+        Bedrock = 255
+    };
+
+}
+
+#endif //CUBE_BLOCK_HPP
