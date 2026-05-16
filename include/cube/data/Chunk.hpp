ef5bce5bd4a4e68d0f4efd8c367eb63304e892f4
Maks Makuta <maksmakuta@proton.me>
2026-05-16 09:20:56 +0300

    [0.4.8] cleanup block enum

--- include/cube/data/Chunk.hpp
+++ include/cube/data/Chunk.hpp
@@ -3,18 +3,18 @@
 
 #include <vector>
 
-#include "cube/core/Block.hpp"
-
 #include <glm/vec3.hpp>
 
 namespace cube {
+
+    using Block = uint8_t;
 
     constexpr auto CHUNK_SIZE = 16;
     constexpr auto CHUNK_LEN = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
 
     class Chunk final {
     public:
-        explicit Chunk(Block fill_block = Block::Air);
+        explicit Chunk(Block fill_block = 0);
 
         Block& at(const glm::ivec3&);
         [[nodiscard]] Block at(const glm::ivec3&) const;
--- src/cube/core/Block.cpp
+++ src/cube/core/Block.cpp
@@ -1,19 +1,5 @@
 #include "cube/core/Block.hpp"
 
-#include <stdexcept>
-#include <vector>
-
 namespace cube {
-
-    const auto BLOCK_INFO = std::vector<BlockInfo>{
-
-    };
-
-    BlockInfo getInfo(const Block& block) {
-        if (const auto index = static_cast<int>(block); index < static_cast<int>(Block::COUNT)) {
-            return BLOCK_INFO[index];
-        }
-        throw std::out_of_range("Block index out of range");
-    }
 
 }
